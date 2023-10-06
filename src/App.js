2aa6cb3a978926b3ae26ed6c03d5c370b21205ba
Gilson Levi <117370090+gilsonlevi@users.noreply.github.com>
2023-10-06 07:39:40 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from './componentes/Cabecalho';
+import CadastroUsuario from './componentes/CadastroUsuario';
+import Rodape from './componentes/Rodape';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Cabecalho logo="imagens/header-logo.png" titulo="Supermercado Favorito"/>
+      <CadastroUsuario/>
+
+      <Rodape logo="imagens/header-logo.png"/>
+    </>
   );
 }
 
---
+++ src/componentes/CadastroUsuario/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CadastroUsuario = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default CadastroUsuario

e19b9e27db4bb7634c2c0dc52e6d3cb8c41a50e0
Gilson Levi <117370090+gilsonlevi@users.noreply.github.com>
2023-10-06 11:50:50 -0300

    Fazendo tela de login e cadastro

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Cabecalho from './componentes/Cabecalho';
 import CadastroUsuario from './componentes/CadastroUsuario';
 import Rodape from './componentes/Rodape';
+import TelaLogin from './componentes/TelaLogin';
 
 function App() {
   return (
     <>
       <Cabecalho logo="imagens/header-logo.png" titulo="Supermercado Favorito"/>
       <CadastroUsuario/>
+      <TelaLogin/>
       <Rodape logo="imagens/header-logo.png"/>
     </>
   );
--- src/componentes/CadastroUsuario/index.js
+++ src/componentes/CadastroUsuario/index.js
@@ -1,9 +1,72 @@
 import React from 'react'
+import styles from './CadastroUsuario.module.css'
+import { useState } from 'react';
 
 function CadastroUsuario() {
-  return(
-    <div>
 
+  const [nomeUsuario, setNomeUsuario] = useState("")
+  const [emailUsuario, setEmailUsuario] = useState("")
+  const [senhaUsuario, setSenhaUsuario] = useState("")
+  const [senhaUsuarioConfirmada, setSenhaConfirmada] = useState("")
+
+  function salvarUsuario(nomeUsuario, emailUsuario, senhaUsuario) {
+    let usuario = {nomeUsuario, emailUsuario, senhaUsuario}
+    console.log(usuario)
+    localStorage.setItem("usuario", JSON.stringify (usuario))
+  }
+
+  function validacaoSenha(senhaUsuario, senhaUsuarioConfirmada) {
+    if(senhaUsuario != senhaUsuarioConfirmada) {
+      alert("As senhas não são iguais")
+    }else{
+      salvarUsuario(emailUsuario, nomeUsuario, senhaUsuario)
+    }
+
+  }
+
+  return(
+    <div className={styles.container}>
+      <h1>Se cadastre no nosso site</h1>
+      <form className={styles.formulario}>
+
+      <label>Nome:</label>
+      <br/>
+      <input
+        type='text'
+        name='nomeUsuario'
+        onChange={(e)=> setNomeUsuario(e.target.value)}
+      ></input>
+      <br/>
+
+      <label>Email:</label>
+      <br/>
+      <input
+        type='email'
+        name='emailUsuario'
+        onChange={(e)=> setEmailUsuario(e.target.value)}
+      ></input>
+       <br/>
+
+      <label>Senha:</label>
+      <br/>
+      <input
+        type='password'
+        name='senhaUsuario'
+        onChange={(e)=> setSenhaUsuario(e.target.value)}
+      ></input>
+       <br/>
+
+      <label>Confirmação da Senha:</label>
+      <br/>
+      <input
+        type='password'
+        name='senhaUsuarioConfirmada'
+        onChange={(e)=> setSenhaConfirmada(e.target.value)}
+      ></input><br/>
+      <input type="button" value="Cadastrar" 
+          onClick={()=> validacaoSenha(senhaUsuario, senhaUsuarioConfirmada)}  />
+      
+      </form>
     </div>
   );
 }

18d5b0c5785a319fb71d160be15b54ad89eab917
gilsonlevi <gilsonlevi328@gmail.com>
2023-12-27 16:51:40 -0300

    Terminando o axios GET e PSOT

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -3,50 +3,47 @@ import styles from "./Conteudo.module.css";
 import axios from "axios";
 
 const Conteudo = () => {
-  const [nomeSite, setNomeSite] = useState("");
-  const [url, setUrl] = useState("");
-  const [importante, setImportante] = useState(false);
+  let [nome, setNome] = useState("");
+  let [url, setUrl] = useState("");
+  let [importante, setImportante] = useState(false); 
   const [favoritos, setFavoritos] = useState([]);
-
-  function salvarFavorito() {
-    favoritos.push({ nomeSite, url, importante });
-    setFavoritos([...favoritos]);
-    localStorage.setItem("favoritos", JSON.stringify(favoritos));
-  }
-
-  // Configuração dos headers que você deseja adicionar
-  const headers = {
-    "Access-Control-Allow-Origin": "http://127.0.0.1:3030 ",
-  };
-
-  // Objeto de configuração com os headers
-  const config = {
-    headers: headers,
-  };
 
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:3030/favoritao", config)
+      .get("http://127.0.0.1:3030/favoritao")
       .then((response) => {
         setFavoritos(response.data);
       })
       .catch((error) => {
-        console.log(favoritos);
         console.error("Error fetching data:", error);
       });
   });
 
+  const handleSubmit = (e) => {
+    // e.preventDefault(); PEDIR PARA DAMASCENO EXPLICAR O PORQUE FUNCIONA SEM
+
+    const newFavorito = { nome, url, importante };
+    axios.post('http://127.0.0.1:3030/favoritao', newFavorito)
+        .then(response => {
+            alert(`Novo favorito criado`);
+        })
+        .catch(error => {
+            alert("Digite o nome e a url")
+        });
+};
+
   return (
     <div className={styles.container}>
       <div className={styles.criarFav}>
-        <h1>Conteudo</h1>
+        <h1>Crie Um Novo Favorito</h1>
+        
         <form className={styles.formulario}>
           <label>Nome do Site</label>
           <br />
           <input
             name="nome_site"
             onChange={(evento) => {
-              setNomeSite(evento.target.value);
+              setNome(evento.target.value);
             }}
           ></input>
           <br />
@@ -73,28 +70,22 @@ const Conteudo = () => {
           <input
             type="button"
             value="Salvar"
-            onClick={() => salvarFavorito()}
+            onClick={() => handleSubmit()}
           />
         </form>
 
         <ul className={styles.lista}>
           {favoritos[0] !== undefined &&
             favoritos.map((favorito) => (
+              <div key={favorito.id}>
               <li style={favorito.importante ? { color: "red" } : {}}>
                 {" "}
-                {favorito.nomeSite} : {favorito.url}:
-                {favorito.importante ? "importante" : ""}{" "}
+                {favorito.nome} : {favorito.url}:
+                {favorito.importante ? " importante" : ""}{" "}
               </li>
+              </div>
             ))}
         </ul>
-
-        <h1>Posts</h1>
-        {favoritos.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))}
       </div>
     </div>
   );
